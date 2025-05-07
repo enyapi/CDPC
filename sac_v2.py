5497ea6cdde96f71f31350c6dc484e501b91bcd9
koIsNice <a0979721156@gmail.com>
2025-05-08 00:35:06 +0800

    add models

--- sac_v2.py
+++ sac_v2.py
@@ -44,7 +44,7 @@ class ValueNetwork(nn.Module):
         
         self.linear1 = nn.Linear(state_dim, hidden_dim)
         self.linear2 = nn.Linear(hidden_dim, hidden_dim)
-        self.linear3 = nn.Linear(hidden_dim, hidden_dim)
+        # self.linear3 = nn.Linear(hidden_dim, hidden_dim)
         self.linear4 = nn.Linear(hidden_dim, 1)
         # weights initialization
         self.linear4.weight.data.uniform_(-init_w, init_w)
@@ -53,7 +53,7 @@ class ValueNetwork(nn.Module):
     def forward(self, state):
         x = F.relu(self.linear1(state))
         x = F.relu(self.linear2(x))
-        x = F.relu(self.linear3(x))
+        # x = F.relu(self.linear3(x))
         x = self.linear4(x)
         return x
         
